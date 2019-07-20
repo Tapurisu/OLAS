57df911790d1308699d92dcdabe68c3557940f45
Tapurisu <51875379+Tapurisu@users.noreply.github.com>
2019-07-20 10:38:53 +0800

    Add files via upload

--- WEB-INF/classes/myservlet/UploadServlet.java
+++ WEB-INF/classes/myservlet/UploadServlet.java
@@ -1,97 +1,82 @@
 package myservlet;
 
+import myservlet.teacher_bean;
+import myservlet.source_bean;
+import myservlet.class_bean;
+
 import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.List;
- 
+import java.util.Collection;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
- 
-import org.apache.commons.fileupload.FileItem;
-import org.apache.commons.fileupload.disk.DiskFileItemFactory;
-import org.apache.commons.fileupload.servlet.ServletFileUpload;
- 
+import javax.servlet.http.Part;
 
-public class UploadServlet extends HttpServlet {
-    private static final long serialVersionUID = 1L;
-     
-    // 上传文件存储目录
-    private static final String UPLOAD_DIRECTORY = "/upload";
- 
-    // 上传配置
-    private static final int MEMORY_THRESHOLD   = 1024 * 1024 * 3;  // 3MB
-    private static final int MAX_FILE_SIZE      = 1024 * 1024 * 40; // 40MB
-    private static final int MAX_REQUEST_SIZE   = 1024 * 1024 * 50; // 50MB
- 
-    protected void doPost(HttpServletRequest request,
-        HttpServletResponse response) throws ServletException, IOException {
-        // 检测是否为多媒体上传
-        if (!ServletFileUpload.isMultipartContent(request)) {
-            // 如果不是则停止
-            PrintWriter writer = response.getWriter();
-            writer.println("Error: 表单必须包含 enctype=multipart/form-data");
-            writer.flush();
-            return;
-        }
- 
-        // 配置上传参数
-        DiskFileItemFactory factory = new DiskFileItemFactory();
-        // 设置内存临界值 - 超过后将产生临时文件并存储于临时目录中
-        factory.setSizeThreshold(MEMORY_THRESHOLD);
-        // 设置临时存储目录
-        factory.setRepository(new File(System.getProperty("java.io.tmpdir")));
- 
-        ServletFileUpload upload = new ServletFileUpload(factory);
-         
-        // 设置最大文件上传值
-        upload.setFileSizeMax(MAX_FILE_SIZE);
-         
-        // 设置最大请求值 (包含文件和表单数据)
-        upload.setSizeMax(MAX_REQUEST_SIZE);
-        
-        // 中文处理
-        upload.setHeaderEncoding("UTF-8"); 
+@WebServlet(name="UploadServlet",urlPatterns="/UploadServlet")
+@MultipartConfig
+public class UploadServlet extends HttpServlet{
 
-        // 构造临时路径来存储上传的文件
-        // 这个路径相对当前应用的目录
-        String uploadPath = getServletContext().getRealPath("/") + File.separator + UPLOAD_DIRECTORY;
-       
-         
-        File uploadDir = new File(uploadPath);
-        if (!uploadDir.exists()) {
-            uploadDir.mkdir();
-        }
- 
-        try {
-            // 解析请求的内容提取文件数据
-            List<FileItem> formItems = upload.parseRequest(request);
- 
-            if (formItems != null && formItems.size() > 0) {
-                // 迭代表单数据
-                for (FileItem item : formItems) {
-                    // 处理不在表单中的字段
-                    if (!item.isFormField()) {
-                        String fileName = new File(item.getName()).getName();
-//                        String filePath = uploadPath + File.separator + fileName;
-			String filePath = uploadPath + "//" + fileName;
-                        File storeFile = new File(filePath);
-                        // 在控制台输出文件的上传路径
-//                        System.out.println(filePath);
-                        // 保存文件到硬盘
-                        item.write(storeFile);
-                        request.setAttribute("message", "文件上传成功!");
-                    }
-                }
-            }
-        } catch (Exception ex) {
-            request.setAttribute("message", "错误信息: " + ex.getMessage());
-        }
-        // 跳转到 message.jsp
-        getServletContext().getRequestDispatcher("/message.jsp").forward(request, response);
-    }
+   public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+             HttpSession session = request.getSession();
+             request.setCharacterEncoding("utf-8");
+             //获得老师将要上传的Cou_ID以及老师的ID
+             String Cou_ID = request.getParameter("R");
+             teacher_bean teacher = (teacher_bean) session.gerAttribute("TeacherBean");
+             String Tea_ID = teacher.getTea_ID();
+
+
+             response.setCharacterEncoding("utf-8");
+             response.setContentType("text/html;charset=utf-8");
+
+             String savePath = "/tmp/uploadFile" + File.separator + Tea_ID + File.separator + Cou_ID;
+
+             Collection<Part> parts = request.getParts();
+
+             if (parts.size()==1) {
+
+                 Part part = request.getPart("file");
+                 String header = part.getHeader("content-disposition");
+
+                 String fileName = getFileName(header);
+
+                 part.write(savePath + File.separator + fileName);
+             }else {
+
+                 for (Part part : parts) {
+                     String header = part.getHeader("content-disposition");
+
+                     String fileName = getFileName(header);
+
+                     part.write(savePath + File.separator + fileName);
+                 }
+             }
+             PrintWriter out = response.getWriter();
+             out.println("Upload Successfully!");
+             out.flush();
+             out.close();
+     }
+
+      /**
+      *
+      *
+      *
+      * @param header
+      * @return
+      */
+     public String getFileName(String header) {
+
+         String[] tempArr1 = header.split(";");
+         String[] tempArr2 = tempArr1[2].split("=");
+         String fileName = tempArr2[1].substring(tempArr2[1].lastIndexOf("\\")+1).replaceAll("\"", "");
+         return fileName;
+     }
+
+     public void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+         this.doGet(request, response);
+     }
 }
-
